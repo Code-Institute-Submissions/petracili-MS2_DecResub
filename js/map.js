93707c9ec00fccda0999c0ce00c95611d399cac5
petracili <petracili7@gmail.com>
2021-03-06 20:55:53 +0000

    add js

--- js/map.js
+++ js/map.js
@@ -31,3 +31,29 @@
        //var markerClusterer = new MarkerClusterer(map, markers,
        // {imagePath:"https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"});
     }
+
+    $('.contact-form').find('.form-control').each(function() {
+  var targetItem = $(this).parent();
+  if ($(this).val()) {
+    $(targetItem).find('label').css({
+      'top': '10px',
+      'fontSize': '14px'
+    });
+  }
+})
+$('.contact-form').find('.form-control').focus(function() {
+  $(this).parent('.input-block').addClass('focus');
+  $(this).parent().find('label').animate({
+    'top': '10px',
+    'fontSize': '14px'
+  }, 300);
+})
+$('.contact-form').find('.form-control').blur(function() {
+  if ($(this).val().length == 0) {
+    $(this).parent('.input-block').removeClass('focus');
+    $(this).parent().find('label').animate({
+      'top': '25px',
+      'fontSize': '18px'
+    }, 300);
+  }
+})
