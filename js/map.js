66ce64247e76bbc6b3c92e0933cfaa7d4993cc0c
petracili <petracili7@gmail.com>
2021-03-13 13:17:19 +0000

    add marker on the google maps

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 // Initialize and add the map
       function initMap() {
         // The location of Uluru
-        const uluru = { lat: -25.344, lng: 131.036 };
+        const uluru = { lat: 53.344307, lng: -6.265476 };
         // The map, centered at Uluru
         const map = new google.maps.Map(document.getElementById("map"), {
           zoom: 4,
