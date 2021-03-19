39469e13ef9535a7093a0c191e899cda42277a3f
petracili <petracili7@gmail.com>
2021-03-19 11:18:15 +0000

    add function

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,10 +1,10 @@
 emailjs.init("user_xKMlyK4S0o5h08XSJvtF7");
 
 function sendMail(contactForm) {
-    emailjs.send("gmail", "template_jg2e7ij", {
-        "from_name" : contactForm.firstname.value + ' ' + contactForm.lastname.value,
+    emailjs.send("service_9nyquja", "template_na9qb6i", {
+        "from_name" : contactForm.name.value,
         "from_email" : contactForm.email.value,
-        "project_request" : contactForm.message.value
+        "project_request" : contactForm.projectsummary.value
     })
     .then(
         function(response){
