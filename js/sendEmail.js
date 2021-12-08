dae73834966992696300d93fba0ec1af389715fd
petracili <petracili7@gmail.com>
2021-12-08 14:33:45 +0000

    Added email for booking

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -7,7 +7,25 @@ function sendMail(contactForm) {
     })
     .then(
         function(response){
-            console.log("SUCCESS", response);
+            alert('Contact sent, please check you email');
+        },
+        function(error){
+             console.log("FAILED", error);
+        });
+
+    return false;
+}
+
+function sendReservation(contactForm) { 
+    emailjs.send("service_9nyquja", "template_na9qb6i", {
+        "gym_location" : contactForm.gym_location.options[contactForm.gym_location.selectedIndex].text,
+        "gym_class" : contactForm.gym_class.options[contactForm.gym_class.selectedIndex].text,
+        "gym_time" : contactForm.gym_time.options[contactForm.gym_time.selectedIndex].text,
+        "from_email" : contactForm.email.value
+    })
+    .then(
+        function(response){
+            alert('Reservation sent, please check you email');
         },
         function(error){
              console.log("FAILED", error);
